4633fc7fffcccdbb287615f68136d266bca9d737
yiyZh <zhangyiy@grinnell.edu>
2025-04-07 21:58:18 -0500

    Update tests for userprofile and categories

--- tests/tests_backend_categories.py
+++ tests/tests_backend_categories.py
@@ -39,37 +39,27 @@ class CategoryAPITest(TestCase):
     def test_list_categories(self):
         url = reverse("category-list")
         response = self.client.get(url)
-        # need fixing the status_code check
-        # self.assertEqual(response.status_code, )
         self.assertEqual(response.data[0]["name"], "Electronics")
 
     def test_retrieve_category(self):
         url = reverse("category-detail", args=[self.category.id])
         response = self.client.get(url)
-        # need fixing the status_code check
-        # self.assertEqual(response.status_code, )
         self.assertEqual(response.data["name"], "Electronics")
 
     def test_create_category(self):
         url = reverse("category-list")
         data = {"name": "Stationery"}
         response = self.client.post(url, data)
-        # need fixing the status_code check
-        # self.assertEqual(response.status_code, )
         self.assertTrue(Category.objects.filter(name="Stationery").exists())
 
     def test_update_category(self):
         url = reverse("category-detail", args=[self.category.id])
         data = {"name": "Updated Electronics"}
         response = self.client.put(url, data)
-        # need fixing the status_code check
-        # self.assertEqual(response.status_code, )
         self.category.refresh_from_db()
         self.assertEqual(self.category.name, "Updated Electronics")
 
     def test_delete_category(self):
         url = reverse("category-detail", args=[self.category.id])
         response = self.client.delete(url)
-        # need fixing the status_code check
-        # self.assertEqual(response.status_code,)
         self.assertFalse(Category.objects.filter(id=self.category.id).exists())
