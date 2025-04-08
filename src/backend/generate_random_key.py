e9fb0842ab297f9166f3e0a3e6e54b68d93bf893
Seunghyeon Kim <seunghk1206@gmail.com>
2025-04-08 15:47:51 -0500

    fixed automated testing

--- src/backend/generate_random_key.py
+++ src/backend/generate_random_key.py
@@ -1,9 +1,14 @@
 #!python3
 from django.core.management.utils import get_random_secret_key  
+import os
 
 new_secret_key = get_random_secret_key()
+AUTH_KEY_DIR = "./AUTHKEY/"
 
-fname = 'AUTHKEY/config.py'
+if not os.path.exists(AUTH_KEY_DIR):
+    os.makedirs(AUTH_KEY_DIR)
+
+fname = AUTH_KEY_DIR+'config.py'
 data = new_secret_key
 
 with open(fname, 'w') as f:
