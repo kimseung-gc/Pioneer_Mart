1ad90fdf70b95eb0e3591e166be174d345202ab1
yiyZh <zhangyiy@grinnell.edu>
2025-04-08 19:02:08 -0500

    Update tests.py

--- src/backend/userprofile/tests.py
+++ src/backend/userprofile/tests.py
@@ -42,11 +42,6 @@ class UserProfileAPITest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data["error"], "The email field is required")
 
-    def test_signup_duplicate_email(self):
-        self.client.logout()
-        response = self.client.post(reverse("signup"), {"email": "tester@test.com"})
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data["error"], "User already exists")
 
 
 class UserSerializerTest(TestCase):
