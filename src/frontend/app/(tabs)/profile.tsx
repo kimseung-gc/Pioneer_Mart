0ea3aedf3d5bd93105afb31f786374355574c9e7
muhammadkhalid-03 <muhammadkhalid200314@gmail.com>
2025-04-09 15:42:55 -0500

    profile screen setup

--- src/frontend/app/(tabs)/profile.tsx
+++ src/frontend/app/(tabs)/profile.tsx
@@ -1,10 +1,75 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, Alert, StyleSheet, ScrollView } from "react-native";
+import { useAuth } from "../contexts/AuthContext";
+import { useUserStore } from "@/stores/userStore";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-export default function ProfileScreen() {
+const ProfileScreen = () => {
+  const { authToken, onLogout } = useAuth();
+  const [isLogoutVisible, setIsLogoutVisible] = useState(false);
+  const insets = useSafeAreaInsets();
+
+  const { userData, isLoading, error, fetchUserData } = useUserStore();
+
+  useEffect(() => {
+    if (authToken) {
+      loadUserProfile();
+    }
+  }, [authToken]);
+
+  const loadUserProfile = async () => {
+    try {
+      await fetchUserData(authToken || "");
+    } catch (error) {
+      Alert.alert("Error", "Failed to load profile. Please try again");
+    }
+  };
+
+  const openLogoutModal = () => {
+    setIsLogoutVisible(true);
+    console.log("Wants to log out...");
+  };
+
+  const closeLogoutModal = () => {
+    setIsLogoutVisible(false);
+    console.log("Doesn't want to log out...");
+  };
+
+  if (isLoading && !userData) {
+    return (
+      <View style={[styles.container, styles.loadingContainer]}>
+        <Text>Loading profile...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home Screen</Text>
-    </View>
+    <ScrollView
+      style={[styles.container, { paddingTop: insets.top }]}
+    ></ScrollView>
   );
-}
+};
+
+export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    padding: 20,
+  },
+  loadingContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    width: "100%",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  headerTitle: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#333",
+  },
+});
