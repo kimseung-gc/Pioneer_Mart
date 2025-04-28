3f2659df0c831b412349a2bf62b0e975071883c4
muhammadkhalid-03 <muhammadkhalid200314@gmail.com>
2025-04-28 18:30:03 -0500

    asgi change to fix app initializing

--- src/backend/backend/asgi.py
+++ src/backend/backend/asgi.py
@@ -8,14 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+django.setup()  # initialize Django before importing any apps
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 # application = get_asgi_application()
 application = ProtocolTypeRouter(
