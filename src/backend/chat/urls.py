af756b77fa8e6e9a7541c70771b885b6d8130744
yiyZh <zhangyiy@grinnell.edu>
2025-05-06 22:08:44 -0500

    update urls and corresponding tests

--- src/backend/chat/urls.py
+++ src/backend/chat/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("chat/rooms/", views.room_list, name="room_list"),
     path("chat/history/<int:room_id>/", views.chat_history, name="chat_history"),
-    path("chat/get-or-create-room/", views.get_or_create_room),
+    path("chat/get-or-create-room/", views.get_or_create_room, name="get_or_create_room"),
     path(
         "chat/rooms/<int:room_id>/mark-read/",
         views.mark_room_as_read,
