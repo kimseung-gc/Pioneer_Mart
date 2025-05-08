e113c9ae7c269eb8a1bec188544cdef765dbbd06
muhammadkhalid-03 <muhammadkhalid200314@gmail.com>
2025-05-08 16:59:25 -0500

    custom pagination for loadItems frontend function

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -115,7 +115,7 @@ REST_FRAMEWORK = {
         "rest_framework.filters.SearchFilter",
         "rest_framework.filters.OrderingFilter",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "items.pagination.HttpsPageNumberPagination",
     "PAGE_SIZE": 10,
     "SEARCH_PARAM": "q",
 }
